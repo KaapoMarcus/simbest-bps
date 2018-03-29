e1d5bc96566d8a6c511bcdc37b0cdf5aa119af69
lunerdiaodiao <85307524@qq.com>
2018-03-29 10:29:54 +0800

    添加注释

--- src/main/java/com/simbest/bps/app/mapper/WFActivityInstModelMapper.java
+++ src/main/java/com/simbest/bps/app/mapper/WFActivityInstModelMapper.java
@@ -3,6 +3,19 @@ package com.simbest.bps.app.mapper;
 import com.simbest.cores.mapper.ILogicMapper;
 import com.simbest.bps.app.model.WFActivityInstModel;
 
+/**
+ * <strong>Title : WFActivityInstModelMapper</strong><br>
+ * <strong>Description : 流程活动数据库持久层</strong><br>
+ * <strong>Create on : 2018/3/29</strong><br>
+ * <strong>Modify on : 2018/3/29</strong><br>
+ * <strong>Copyright (C) Ltd.</strong><br>
+ *
+ * @author LJW lijianwu@simbest.com.cn
+ * @version <strong>V1.0.0</strong><br>
+ *          <strong>修改历史:</strong><br>
+ *          修改人 修改日期 修改描述<br>
+ *          -------------------------------------------<br>
+ */
 public interface WFActivityInstModelMapper extends ILogicMapper<WFActivityInstModel,Long>{
 
 	
--- src/main/java/com/simbest/bps/app/mapper/WFProcessInstModelMapper.java
+++ src/main/java/com/simbest/bps/app/mapper/WFProcessInstModelMapper.java
@@ -1,13 +1,26 @@
 package com.simbest.bps.app.mapper;
 
+import com.simbest.bps.app.model.WFProcessInstModel;
+import com.simbest.cores.mapper.ILogicMapper;
+
 import java.util.Map;
 
-import com.simbest.cores.mapper.ILogicMapper;
-import com.simbest.bps.app.model.WFProcessInstModel;
-
+/**
+ * <strong>Title : WFProcessInstModelMapper</strong><br>
+ * <strong>Description : 流程实例数据库持久层</strong><br>
+ * <strong>Create on : 2018/3/29</strong><br>
+ * <strong>Modify on : 2018/3/29</strong><br>
+ * <strong>Copyright (C) Ltd.</strong><br>
+ *
+ * @author LJW lijianwu@simbest.com.cn
+ * @version <strong>V1.0.0</strong><br>
+ *          <strong>修改历史:</strong><br>
+ *          修改人 修改日期 修改描述<br>
+ *          -------------------------------------------<br>
+ */
 public interface WFProcessInstModelMapper  extends ILogicMapper<WFProcessInstModel,Long>{
 
-	public WFProcessInstModel getByProcessInst(Long processInstID);
+    WFProcessInstModel getByProcessInst(Long processInstID);
 	
 	void updateTitleByInstID(Map map);
 	
